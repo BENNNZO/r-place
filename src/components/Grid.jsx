620b9be136b800a0fdad5088e436f4cc41da5cd2
Benjamin Phillips <benjamingt537@gmail.com>
2023-08-01 10:38:44 -0400

    added limited drag so you cant loose the canvas and added an fade in animation

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,19 +1,23 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Grid() {
     // const [gridState, setGridState] = useState([])
+    const [windowDim, setWindowDim] = useState({ x: 0, y: 0 })
+
     const [prevGridPos, setPrevGridPos] = useState({ x: 0, y: 0 })
     const [gridPos, setGridPos] = useState({ x: 0, y: 0 })
 
     const [mouseStart, setMouseStart] = useState(0)
     const [moveState, setMoveState] = useState(false)
 
-    const [scale, setScale] = useState(1)
+    const [scale, setScale] = useState(5)
 
-    const WIDTH = 375
-    const HEIGHT = 250
+    const WIDTH = 100
+    const HEIGHT = 100
+
+    const canvasRef = useRef()
 
     useEffect(() => {
         var canvas = document.getElementById("canvas");
@@ -24,7 +28,7 @@ export default function Grid() {
 
         for (let x = 0; x < WIDTH; x++) {
             for (let y = 0; y < HEIGHT; y++) {
-                context.fillStyle = `rgb(${255 / WIDTH * x * 35 % 255}, ${255 / HEIGHT * y * 35 % 255}, 0)`;
+                context.fillStyle = `rgb(255, ${255 / HEIGHT * y * 5 % 255}, ${255 / WIDTH * x * 5 % 255})`;
                 // context.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
                 context.fillRect(widthConst * x, heightConst * y, widthConst, heightConst);
             }
@@ -32,6 +36,12 @@ export default function Grid() {
 
         context.translate(0.5, 0.5)
         context.imageSmoothinEnabled = false
+
+        window.addEventListener("resize", e => {
+            setWindowDim({ x: window.innerWidth, y: window.innerHeight })
+        })
+        setWindowDim({ x: window.innerWidth, y: window.innerHeight })
+        setGridPos({ x: (window.innerWidth / 2) - (canvasRef.current.clientWidth / 2), y: (window.innerHeight / 2) - (canvasRef.current.clientHeight / 2) })
     }, [])
 
     function handleMouseDown(e) {
@@ -46,8 +56,16 @@ export default function Grid() {
     }
 
     function handleMouseMove(e) {
+        let xMin = windowDim.x / 2 - canvasRef.current.clientWidth
+        let xMax = windowDim.x / 2
+        let yMin = windowDim.y / 2 - canvasRef.current.clientHeight 
+        let yMax = windowDim.y / 2 
+
         if (moveState === true) {
-            setGridPos({ x: prevGridPos.x - (mouseStart.x - e.clientX), y: prevGridPos.y - (mouseStart.y - e.clientY) })
+            setGridPos({ 
+                x: Math.max(xMin, Math.min(xMax, prevGridPos.x - (mouseStart.x - e.clientX))), 
+                y: Math.max(yMin, Math.min(yMax, prevGridPos.y - (mouseStart.y - e.clientY)))
+            })
         }
     }
 
@@ -56,13 +74,13 @@ export default function Grid() {
         let ys = (e.clientY - gridPos.y) / scale
 
             if (e.deltaY < 0) {
-                // if (scale * 1.25 < 20) {
+                if (scale * 1.25 < 150) {
                     console.log("up")
                     setScale(prev => prev *= 1.25)
                     setGridPos({ x: e.clientX - xs * (scale * 1.25), y: e.clientY - ys * (scale * 1.25) })
-                // }
+                }
             } else {
-                if (scale / 1.25 > 1) {
+                if (scale / 1.25 > 4) {
                     console.log("down")
                     setScale(prev => prev /= 1.25)
                     setGridPos({ x: e.clientX - xs * (scale / 1.25), y: e.clientY - ys * (scale / 1.25) })
@@ -72,7 +90,7 @@ export default function Grid() {
 
     return (
         <section 
-            className='w-full h-full border-2 border-red-400 relative cursor-move'
+            className='w-full h-full relative cursor-move'
             onMouseDown={e => handleMouseDown(e)}
             onMouseUp={e => handleMouseUp(e)}
             onMouseMove={e => handleMouseMove(e)}
@@ -84,6 +102,7 @@ export default function Grid() {
                 <p>{JSON.stringify(mouseStart)}</p>
                 <p>{JSON.stringify(moveState)}</p>
                 <p>{JSON.stringify(scale)}</p>
+                <p>{JSON.stringify(windowDim)}</p>
             </div>
             <div 
                 className='absolute'
@@ -91,19 +110,15 @@ export default function Grid() {
                     transform: `translate(${gridPos.x}px, ${gridPos.y}px)`
                 }}    
             >
-                <canvas id='canvas' className='border-2 border-white rounded-md' width={7500} height={5000} style={{ width: 750 * scale, height: 500 * scale, imageRendering: "crisp-edges" }}>
-
-
+                <canvas
+                    ref={canvasRef}
+                    id='canvas'
+                    className='rounded-md canvas'
+                    width={5000} 
+                    height={5000}
+                    style={{ width: 100 * scale, height: 100 * scale }}
+                >
                 </canvas>
-                {/* {gridState.map(e => (
-                    <div className='flex flex-row pointer-events-none' draggable={false}>
-                        {e.map(e => (
-                            <span className='w-3 h-3 bg-white border border-red-400 pointer-events-none' draggable={false}>
-
-                            </span>
-                        ))}
-                    </div>
-                ))} */}
             </div>
         </section>
     )
