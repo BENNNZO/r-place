91fb2bdc9723c518cf0e44abe00e91324388ed2f
Benjamin Phillips <benjamingt537@gmail.com>
2023-08-04 18:05:40 -0400

    cant get the selector to work

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,91 +3,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 export default function Grid(props) {
-    // const [gridState, setGridState] = useState([])
-    // const [windowDim, setWindowDim] = useState({ x: 0, y: 0 })
-
-    // const [prevGridPos, setPrevGridPos] = useState({ x: 0, y: 0 })
-    // const [gridPos, setGridPos] = useState({ x: 0, y: 0 })
-
-    // const [mouseStart, setMouseStart] = useState(0)
-    // const [moveState, setMoveState] = useState(false)
-
-    // const [scale, setScale] = useState(5)
-
-    // const WIDTH = 100
-    // const HEIGHT = 100
-
-    // const canvasRef = useRef()
-
-    // useEffect(() => {
-    //     var canvas = document.getElementById("canvas");
-    //     var context = canvas.getContext("2d");
-
-    //     let widthConst = canvas.width / WIDTH
-    //     let heightConst = canvas.height / HEIGHT
-
-    //     for (let x = 0; x < WIDTH; x++) {
-    //         for (let y = 0; y < HEIGHT; y++) {
-    //             context.fillStyle = `rgb(${255 / HEIGHT * y}, ${255 / WIDTH * x}, 0 )`;
-    //             // context.fillStyle = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
-    //             context.fillRect(widthConst * x, heightConst * y, widthConst / 1.5, heightConst / 1.5);
-    //         }
-    //     }
-
-    //     context.translate(0.5, 0.5)
-    //     context.imageSmoothinEnabled = false
-
-    //     window.addEventListener("resize", e => {
-    //         setWindowDim({ x: window.innerWidth, y: window.innerHeight })
-    //     })
-    //     setWindowDim({ x: window.innerWidth, y: window.innerHeight })
-    //     setGridPos({ x: (window.innerWidth / 2) - (canvasRef.current.clientWidth / 2), y: (window.innerHeight / 2) - (canvasRef.current.clientHeight / 2) })
-    // }, [])
-
-    // function handleMouseDown(e) {
-    //     setMoveState(true)
-    //     setMouseStart({ x: e.clientX, y: e.clientY })
-    //     setPrevGridPos({ x: gridPos.x, y: gridPos.y })
-    // }
-
-    // function handleMouseUp(e) {
-    //     setMoveState(false)
-    //     setMouseStart({ x: e.clientX, y: e.clientY })
-    // }
-
-    // function handleMouseMove(e) {
-    //     let xMin = windowDim.x / 2 - canvasRef.current.clientWidth
-    //     let xMax = windowDim.x / 2
-    //     let yMin = windowDim.y / 2 - canvasRef.current.clientHeight 
-    //     let yMax = windowDim.y / 2 
-
-    //     if (moveState === true) {
-    //         setGridPos({ 
-    //             x: Math.max(xMin, Math.min(xMax, prevGridPos.x - (mouseStart.x - e.clientX))), 
-    //             y: Math.max(yMin, Math.min(yMax, prevGridPos.y - (mouseStart.y - e.clientY)))
-    //         })
-    //     }
-    // }
-
-    // function handleOnWheel(e) {
-    //     let xs = (e.clientX - gridPos.x) / scale
-    //     let ys = (e.clientY - gridPos.y) / scale
-
-    //         if (e.deltaY < 0) {
-    //             if (scale * 1.25 < 150) {
-    //                 console.log("up")
-    //                 setScale(prev => prev *= 1.25)
-    //                 setGridPos({ x: e.clientX - xs * (scale * 1.25), y: e.clientY - ys * (scale * 1.25) })
-    //             }
-    //         } else {
-    //             if (scale / 1.25 > 4) {
-    //                 console.log("down")
-    //                 setScale(prev => prev /= 1.25)
-    //                 setGridPos({ x: e.clientX - xs * (scale / 1.25), y: e.clientY - ys * (scale / 1.25) })
-    //             }
-    //         }
-    // }
-
     return (
         <section 
             className='w-full h-full relative cursor-move'
@@ -96,14 +11,6 @@ export default function Grid(props) {
             onMouseMove={e => props.handleMouseMove(e)}
             onWheel={e => props.handleOnWheel(e)}
         >
-            {/* <div className=' select-none absolute top-0 left-1/2'>
-                <p>{JSON.stringify(gridPos)}</p>
-                <p>{JSON.stringify(prevGridPos)}</p>
-                <p>{JSON.stringify(mouseStart)}</p>
-                <p>{JSON.stringify(moveState)}</p>
-                <p>{JSON.stringify(scale)}</p>
-                <p>{JSON.stringify(windowDim)}</p>
-            </div> */}
             <div 
                 className='absolute'
                 style={{
@@ -114,8 +21,8 @@ export default function Grid(props) {
                     ref={props.canvasRef}
                     id='canvas'
                     className='rounded-md canvas shadow-md'
-                    width={5000} 
-                    height={5000}
+                    width={5050} 
+                    height={5050}
                     style={{ width: 100 * props.scale, height: 100 * props.scale }}
                 >
                 </canvas>
--- src/components/TileSelector.jsx
+++ src/components/TileSelector.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+"use client"
+
+import React, { useState, useEffect } from 'react';
 
 export default function TileSelector(props) {
+    const [box, setBox] = useState({ width: 50, height: 50 })
+
+    useEffect(() => {
+        setBox({ width: props.selectorRef.current.clientWidth, height: props.selectorRef.current.clientHeight })
+        // console.log(props.selectorRef)
+    }, [props.scale])
+
     return (
         <section 
-            className='w-screen h-screen absolute top-0 left-0 pointer-events-none'
+            className='absolute top-0 left-0 pointer-events-none grid place-items-center w-screen h-screen'
             style={{
-                transform: `translate(${props.gridPos.x % props.selectorRef.current.clientWidth}px, ${props.gridPos.y % props.selectorRef.current.clientHeight}px)`
+                transform: `translate(${props.gridPos.x % props.scale}px, ${props.gridPos.y % props.scale}px)`
             }} 
         >
             <div ref={props.selectorRef} className='bg-white' style={{ width: props.scale, height: props.scale }}></div>
